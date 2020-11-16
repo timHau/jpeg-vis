d82e936757b001d0bfd111f624c460f25eaeae41
timHau <tim.hau@hotmail.de>
2020-11-16 19:09:36 +0100

    add intro

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Title from './Title.js';
+import Intro from './Intro.js';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <Title/>
+      <Intro/>
     </div>
   );
 }
---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import './Title.css';
+
+export default function Title() {
+  return (
+    <div>
+      <h1 className="title">JPEG Format</h1>
+      <h5 className="subtitle">Seminar Datenkompression</h5>
+      <h5 className="subtitle">Wintersemester 2020 / 2021, Dr. The Anh Vuong</h5>
+      <h5 className="author">Tim Hau, 6805665</h5>
+    </div>
+  );
+}
