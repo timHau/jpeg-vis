1057f5bdbd07dd6de91e29a94512c3d4bfe67e4a
timHau <tim.hau@hotmail.de>
2020-11-16 20:22:35 +0100

    start with rgb to ycbcr transform

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
     <div className="App">
       <Title/>
       <Intro/>
-      <ColorTransform img={mountain}/>
+      <ColorTransform imgSrc={mountain}/>
     </div>
   );
 }
--- src/ColorTransform.js
+++ src/ColorTransform.js
@@ -1,11 +1,45 @@
+import { useRef, useEffect } from 'react';
 import { BlockMath } from 'react-katex';
 import './ColorTransform.css'
 
+
+function rgbFromImg(img) {
+    const canvas = document.createElement('canvas');
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+    const context = canvas.getContext('2d');
+    context.drawImage(img, 0, 0);
+    const { data } = context.getImageData(0, 0, canvas.width, canvas.height);
+    return data;
+}
+
+function rgbToYCbCr(data) {
+    const res = [];
+    for (let i = 0; i < data.length; i += 4) {
+        const [r, g, b] = [data[i], data[i+1], data[i+2]];
+        const y = 0.299 * r + 0.587 * g + 0.114 * b;
+        const cb = 128 + (-0.168736) * r + (-0.331264) * g + 0.5 * b;
+        const cr = 128 + 0.5 * r + (-0.331264) * g + (-0.081312) * b;
+        res.push(y, cb, cr)
+    }
+    return res
+}
+
 export default function ColorTransform(props) {
-    const { img } = props;
+    const imgEl = useRef();
+    const outputCan = useRef();
+    useEffect(() => {
+        const rgbData = rgbFromImg(imgEl.current)
+        const ycbcrData = rgbToYCbCr(rgbData);
+        console.log(ycbcrData);
+    })
+    const { imgSrc } = props
     return <div>
         <h4 className="chapter-intro">Farbmodellumrechnung</h4>
-        <img className="original" src={img} alt="original" />
+        <div className="ycbcr-container">
+            <img className="original" src={imgSrc} alt="original" ref={imgEl}/>
+            <canvas className="ycbcr-output"/>
+        </div>
         <BlockMath
             math={`
                     \\begin{bmatrix} Y \\\\ Cb \\\\ Cr \\end{bmatrix} \\approx
