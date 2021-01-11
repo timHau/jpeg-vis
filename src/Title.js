71b4341f3f081f3d82063520324d6abe8eed1e60
tim hau <tim.hau@hotmail.de>
2021-01-11 11:19:51 +0100

    format --> komprimierung

--- src/Title.js
+++ src/Title.js
@@ -3,7 +3,7 @@ import './Title.css';
 export default function Title() {
   return (
     <div>
-      <h1 className="title">JPEG Format</h1>
+      <h1 className="title">JPEG Komprimierung</h1>
       <h5 className="subtitle">Seminar Datenkompression</h5>
       <h5 className="subtitle">Wintersemester 2020 / 2021, Dr. The Anh Vuong</h5>
       <h5 className="author">Tim Hau, 6805665</h5>
