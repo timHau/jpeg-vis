9346c1d9286c7fb022fb173ccfc68a97c310fc7e
tim hau <tim.hau@hotmail.de>
2021-01-11 11:13:46 +0100

    make resource links clickable

--- src/Resources.js
+++ src/Resources.js
@@ -4,11 +4,11 @@ export default function Resources() {
     return <div>
         <h4 className="chapter-intro">Quellen</h4>
         <ol>
-            <li>http://weitz.de/dct/</li>
-            <li>http://www-lehre.inf.uos.de/nwa/mm/vortrag/node31.html</li>
-            <li>https://de.wikipedia.org/wiki/JPEG</li>
-            <li>https://de.wikipedia.org/wiki/Diskrete_Kosinustransformation</li>
-            <li>https://www.tu-chemnitz.de/informatik/ThIS/downloads/courses/ws02/datkom/JPEG_Kompression.pdf</li>
+            <li><a href="http://weitz.de/dct/">http://weitz.de/dct/</a></li>
+            <li><a href="http://www-lehre.inf.uos.de/nwa/mm/vortrag/node31.html">http://www-lehre.inf.uos.de/nwa/mm/vortrag/node31.html</a></li>
+            <li><a href="https://de.wikipedia.org/wiki/JPEG">https://de.wikipedia.org/wiki/JPEG</a></li>
+            <li><a href="https://de.wikipedia.org/wiki/Diskrete_Kosinustransformation">https://de.wikipedia.org/wiki/Diskrete_Kosinustransformation</a></li>
+            <li><a href="https://www.tu-chemnitz.de/informatik/ThIS/downloads/courses/ws02/datkom/JPEG_Kompression.pdf">https://www.tu-chemnitz.de/informatik/ThIS/downloads/courses/ws02/datkom/JPEG_Kompression.pdf</a></li>
         </ol>
     </div>
 }
